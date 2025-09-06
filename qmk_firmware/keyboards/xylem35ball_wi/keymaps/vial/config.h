302985dcc0e0787c6de8ffa0038f20bfb9973446
wdknt <44008511+wdknt@users.noreply.github.com>
2025-09-06 14:09:29 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/xylem35ball_wi/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xcd, 0x28, 0xc1, 0xff, 0xee, 0xf0, 0xe7, 0x19}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+#define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_INVERT_Y
